7beebf14af94cfb378ff1c5b30167d3081097483
Aayush <koiralaaayush20@gmail.com>
2026-06-01 22:48:01 -0600

    Day 1 Introduction

---
+++ Day1Introduction.py
@@ -0,0 +1,23 @@
+#Introduction
+
+#Day 1 of 30 days of code
+
+print("Hello world")
+print(2 + 3) # Addition
+print (3 - 1) # Subtraction
+print (2 * 3) # Multiplication
+print (10 / 2) # Division
+print ( 3 ** 2) # Exponentiation
+print ( 10 % 3) # Modulus
+print (3 // 2) # Floor division
+
+# Checking the data type
+print(type("Hello world")) # String
+print(type(10)) # Integer   
+print(type(3.14)) # Float
+print(type(True)) # Boolean
+print(type([1, 2, 3])) # List
+print(type({'name': 'Aayush'})) # Dictionary
+
+
+
